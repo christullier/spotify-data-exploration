dfbe85d8c1f5c4c07c1ea0e50f5f7a0d307b7c5c
Chris Tullier <christopher.tullier98@gmail.com>
2024-09-25 13:50:30 -0400

    typing and comments

--- none.py
+++ none.py
@@ -5,8 +5,8 @@ from get_json import get_json_data
 data = get_json_data()
 import os
 
-missing_dates = {}
-missing = []
+missing_dates: dict[str, int] = {}
+missing: list[tuple[str, str]] = []
 
 
 for song in data:
@@ -14,18 +14,20 @@ for song in data:
     podcast = song.get("episode_show_name")
     artist = song.get("master_metadata_album_artist_name")
     # if (song == None) & (podcast == None):
+
     if not artist and not podcast:
+        # Get the date of the song (timestamp)
         date = song.get("ts").split("T")[0]
+
+        # Increment the count for this date in the missing_dates dictionary
+        # If this date is new, initialize its count to 1; otherwise, increment the existing count
         if date not in missing_dates.keys():
             missing_dates[date] = 1
         else:
             missing_dates[date] += 1
 
+        # Add this song to the list of missing songs
         missing.append(song)
-        # print(song)
-        # input()
-        # os.system("clear")
-
 
 for date in missing_dates:
     if missing_dates[date] > 5:
