35cfda5f831d55b8c16b13a5691861e16aa041f2
Chris Tullier <christopher.tullier98@gmail.com>
2025-03-26 13:41:41 -0400

    file rename

---
+++ get_data.py
@@ -0,0 +1,65 @@
+import json
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SPOTIFY_DIR = getenv("SPOTIFY_DIR")
+APPLE_DIR = getenv("APPLE_DIR")
+
+
+def read_json(file_names):
+    json_data = []
+    for json_file in file_names:
+        print(f"{json_file=}")
+        with open(json_file) as f:
+            new_data = json.load(f)
+            json_data.extend(new_data)
+
+    return json_data
+
+
+def read_csv(file_names):
+    csv_data = []
+    for json_file in file_names:
+        print(f"{json_file=}")
+        with open(json_file) as f:
+            new_data = f.readlines()
+            csv_data.extend(new_data)
+
+    return csv_data
+
+
+def get_spotify_json():
+    files = [
+        "Streaming_History_Audio_2013-2019_0.json",
+        "Streaming_History_Audio_2019_1.json",
+        "Streaming_History_Audio_2019-2020_2.json",
+        "Streaming_History_Audio_2020-2021_3.json",
+        "Streaming_History_Audio_2021_4.json",
+        "Streaming_History_Audio_2021-2023_5.json",
+        "Streaming_History_Audio_2023-2024_6.json",
+        # "Streaming_History_Video_2016-2024.json",
+    ]
+
+    file_paths = [f"{SPOTIFY_DIR}/{file}" for file in files]
+
+    json_data = read_json(file_paths)
+    return json_data
+
+
+def get_apple_json():
+    json_data = []
+    files = [
+        "Apple Music Play Activity.csv",
+    ]
+    file_paths = [f"{APPLE_DIR}/{file}" for file in files]
+    json_data = read_csv(file_paths)
+    return json_data
+
+
+if __name__ == "__main__":
+    # d = get_spotify_json()
+    d = get_apple_json()
+    print(len(d))
