f887faaa8f39d40e98ff73acfbcfa4672e923e53
Chris Tullier <christopher.tullier98@gmail.com>
2025-03-28 14:21:37 -0500

    Add DEBUG flag to control logging in read_json function and clean up commented code in get_data.py

--- get_data.py
+++ get_data.py
@@ -10,11 +10,14 @@ load_dotenv()
 SPOTIFY_DIR = getenv("SPOTIFY_DIR")
 APPLE_DIR = getenv("APPLE_DIR")
 
+DEBUG = False
+
 
 def read_json(file_names):
     json_data = []
     for json_file in file_names:
-        print(f"{json_file=}")
+        if DEBUG:
+            print(f"{json_file=}")
         with open(json_file) as f:
             new_data = json.load(f)
             json_data.extend(new_data)
@@ -23,15 +26,12 @@ def read_json(file_names):
 
 
 def read_csv(file_names):
-    # csv_data = []
     for csv_filename in file_names:
         df = pd.read_csv(csv_filename)
-        # csv_data.extend(df)
     return df
 
 
 def get_spotify():
-
     # Get all Streaming_History_Audio_*.json files from SPOTIFY_DIR
     files = glob.glob(path.join(SPOTIFY_DIR, "Streaming_History_Audio_*.json"))
     file_paths = files
